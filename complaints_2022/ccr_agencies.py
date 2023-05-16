7ef425b3adbde328675a31cf40606b5858b13e73
Sasa Djukic <djukicmsasa@gmail.com>
2023-05-16 09:52:26 +0200

    add help module to store names of credit reporting agencies

---
+++ complaints_2022/ccr_agencies.py
@@ -0,0 +1,8 @@
+
+
+# store names of consumer credit reporting agencies as they appear in the data sheet
+ccra = [
+        'TRANSUNION INTERMEDIATE HOLDINGS, INC.', 
+        'Experian Information Solutions Inc.',
+        'EQUIFAX, INC.'
+        ]
